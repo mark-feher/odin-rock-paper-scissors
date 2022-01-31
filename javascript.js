babbc4b12a22d55c3970137e46e5de9fe02b4fce
mark-feher <maark.feheer@gmail.com>
2022-01-31 10:34:47 +0100

    Rewrote entire js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,32 @@
 function computerPlay () {
-    return Math.floor(Math.random() * 3);
+    let rockPaperScissors = ["Rock", "Paper", "Scissors"];
+
+    return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)]
 }
+
+function playerPlay (playerSelection) {
+    playerSelection = prompt();
+    return playerSelection;
+}
+
+function playOneRound (playerSelection, computerSelection) {
+    console.log("Player chose: " + playerSelection + "\n"
+    + "Computer chose: " + computerSelection);
+    if (playerSelection === computerSelection) {
+        return "Tie!";
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "Paper beats Rock, you lose!";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "Rock beats Scissors, you win!";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "Paper beats Rock, you win!";
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "Scissors beat Rock, you lose!";
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "Rock beats Scissors, you lose!";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "Scissors beat Paper, you win!";
+    }
+}
+
+// console.log(playOneRound(playerPlay(), computerPlay()));
